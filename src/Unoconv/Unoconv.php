98fc0c7a3ecabf84fef8a054784395723ae77aba
Oleksandr Grygorash <ogrygorash@gmail.com>
2020-01-20 21:34:12 -0800

    Add stdout output.

--- src/Unoconv/Unoconv.php
+++ src/Unoconv/Unoconv.php
@@ -21,75 +21,98 @@ use Unoconv\Exception\InvalidFileArgumentException;
 
 class Unoconv extends AbstractBinary
 {
-    const FORMAT_PDF = 'pdf';
+  const FORMAT_PDF = 'pdf';
 
-    /**
-     * {@inheritdoc}
-     */
-    public function getName()
-    {
-        return 'Unoconv';
+  /**
+   * Command output.
+   *
+   * @var string
+   */
+  protected $output;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getName()
+  {
+    return 'Unoconv';
+  }
+
+  /**
+   * Get stdout output
+   */
+  public function getOutput()
+  {
+    return $this->output;
+  }
+
+  /**
+   * Converts a file to another format
+   *
+   * @param string $input      The path to the input file
+   * @param string $format     The output format
+   * @param string $outputFile The path to the output file
+   * @param string $pageRange  The range of pages. 1-14 for pages 1 to 14
+   *
+   * @return Unoconv
+   *
+   * @throws InvalidFileArgumentException In case the input file is not readable or does not exist
+   * @throws RuntimeException             In case the output file can not be written, or the process failes
+   */
+  public function convert($input, $format, $outputFile = null, $pageRange = null)
+  {
+    if (!file_exists($input)) {
+      throw new InvalidFileArgumentException(sprintf('File %s does not exists', $input));
     }
 
-    /**
-     * Converts a file to another format
-     *
-     * @param string $input      The path to the input file
-     * @param string $format     The output format
-     * @param string $outputFile The path to the output file
-     * @param string $pageRange  The range of pages. 1-14 for pages 1 to 14
-     *
-     * @return Unoconv
-     *
-     * @throws InvalidFileArgumentException In case the input file is not readable or does not exist
-     * @throws RuntimeException             In case the output file can not be written, or the process failes
-     */
-    public function convert($input, $format, $outputFile, $pageRange = null)
-    {
-        if (!file_exists($input)) {
-            throw new InvalidFileArgumentException(sprintf('File %s does not exists', $input));
-        }
+    $stdout = FALSE;
 
-        $arguments = array(
-            '--format=' . $format,
-            '--output=' . $outputFile,
-        );
+    $arguments = [
+      "--format=$format",
+    ];
 
-        if (preg_match('/\d+-\d+/', $pageRange)) {
-            $arguments[] = '-e';
-            $arguments[] = 'PageRange=' . $pageRange;
-        }
-
-        $arguments[] = $input;
-
-        try {
-            $this->command($arguments);
-        } catch (ExecutionFailureException $e) {
-            throw new RuntimeException(
-                'Unoconv failed to convert file', $e->getCode(), $e
-            );
-        }
-
-
-        return $this;
+    if (!empty($outputFile)) {
+      $arguments[]= "--output=$outputFile";
+    }
+    else {
+      $arguments[]= '--stdout';
+      $stdout = TRUE;
     }
 
-    /**
-     * Creates an Unoconv instance
-     *
-     * @param LoggerInterface              $logger
-     * @param array|ConfigurationInterface $conf
-     *
-     * @return Unoconv
-     */
-    public static function create($conf = array(), LoggerInterface $logger = null)
-    {
-        if (!$conf instanceof ConfigurationInterface) {
-            $conf = new Configuration($conf);
-        }
-
-        $binaries = $conf->get('unoconv.binaries', array('unoconv'));
-
-        return static::load($binaries, $logger, $conf);
+    if (preg_match('/\d+-\d+/', $pageRange)) {
+      $arguments[] = '-e';
+      $arguments[] = "PageRange=$pageRange";
     }
+
+    $arguments[] = $input;
+
+    try {
+      $this->output = $this->command($arguments);
+    } catch (ExecutionFailureException $e) {
+      throw new RuntimeException(
+        'Unoconv failed to convert file', $e->getCode(), $e
+      );
+    }
+
+    return $this;
+  }
+
+  /**
+   * Creates an Unoconv instance
+   *
+   * @param LoggerInterface              $logger
+   * @param array|ConfigurationInterface $conf
+   *
+   * @return Unoconv
+   */
+  public static function create($conf = array(), LoggerInterface $logger = null)
+  {
+    if (!$conf instanceof ConfigurationInterface) {
+      $conf = new Configuration($conf);
+    }
+
+    $binaries = $conf->get('unoconv.binaries', array('unoconv'));
+
+    return static::load($binaries, $logger, $conf);
+  }
 }
